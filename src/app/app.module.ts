690374d41bfd70846fa23912558620e329b6cfd8
bmribeiro <bruno.miguel.rib@gmail.com>
2024-07-28 15:01:16 +0100

    Angular Material ; Bootstrap ; Book (Component and Service) - Starter Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './pages/books/books.component';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/books/books.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent implements OnInit{
+
+  books$!: Observable<any[]>;
+
+  constructor(private bookService : BookService){
+
+  }
+  ngOnInit(): void {
+    this.books$ = this.bookService.getBooks();
+  }
+
+}
