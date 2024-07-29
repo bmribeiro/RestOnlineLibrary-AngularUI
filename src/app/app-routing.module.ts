0da2f6aacbdf877927a708e3fb190504be931c62
bmribeiro <bruno.miguel.rib@gmail.com>
2024-07-29 19:17:42 +0100

    Routes and Not Found Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BooksComponent } from './pages/books/books.component';
+import { UsersComponent } from './pages/users/users.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Home', component: HomeComponent },
+  { path: 'books', title: 'Books', component: BooksComponent },
+  { path: 'users', title: 'Users', component: UsersComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { BooksComponent } from './pages/books/books.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { UsersComponent } from './pages/users/users.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    UsersComponent
+    UsersComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     HttpClientModule,
